1d08ef06f226701d6ee5c0c9448f767f5bc66688
ROHANUR RAHMAN <mdrohanurrahmanontu@gmail.com>
2024-02-20 01:09:41 +0600

    animated checklist added

--- lib/widgets/list.dart
+++ lib/widgets/list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:msh_checkbox/msh_checkbox.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_provider/provider/todo_provider.dart';
 
@@ -12,6 +13,11 @@ class listTile extends StatelessWidget{
         itemBuilder: (context, index){
           return ListTile(
             title: Text(dataModel.allToDo[index].title),
+            leading: MSHCheckbox(
+              value: dataModel.allToDo[index].isDone,
+              onChanged: (value){},
+            ),
+            trailing: TextButton(onPressed: (){},child: Text('Delete',style: TextStyle(color: Colors.red,fontWeight: FontWeight.w900),),),
 
           );
         },
